4ea1eda5bd63ee0b86c741cb434c6d5667363fad
ivaylo <iikirilov@gmail.com>
2018-10-04 14:18:38 +0100

    deploy greeter

---
+++ src/main/java/io/blk/jaxlondon/HelloBlockchainWorld.java
@@ -0,0 +1,38 @@
+package io.blk.jaxlondon;
+
+import io.blk.contracts.generated.Greeter;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+public class HelloBlockchainWorld {
+
+    public static void main(String[] args) throws Exception {
+        Web3j web3 = Web3j.build(new HttpService(
+                "https://rinkeby.infura.io/<infura token>"));
+
+        Credentials credentials =
+                WalletUtils.loadCredentials(
+                        "<password>",
+                        "<walletfile>.json");
+
+        Greeter contract = Greeter.deploy(
+                web3, credentials,
+                DefaultGasProvider.GAS_PRICE,
+                DefaultGasProvider.GAS_LIMIT,
+                "Hello blockchain world!").send();
+
+        String greeting = contract.greet().send();
+        System.out.println(greeting);
+
+        TransactionReceipt transactionReceipt =
+                contract.newGreeting("Hello new blockchain world!").send();
+        System.out.println(transactionReceipt.getTransactionHash());
+
+        String newGreeting = contract.greet().send();
+        System.out.println(newGreeting);
+    }
+}
