f648cdacfe2ee55b400365d738b8b9d1ef9c800e
Antony Denyer <git@antonydenyer.co.uk>
2018-11-18 14:32:11 +0000

    auth: wire up Network Version to use epirus basic auth

--- src/main/java/io/blk/jaxlondon/JaxTokenExample.java
+++ src/main/java/io/blk/jaxlondon/JaxTokenExample.java
@@ -2,6 +2,7 @@ package io.blk.jaxlondon;
 
 import io.blk.contracts.generated.JaxToken;
 
+import okhttp3.OkHttpClient;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
@@ -13,8 +14,17 @@ import org.web3j.tx.gas.StaticGasProvider;
 public class JaxTokenExample {
 
     public static void main(String[] args) throws Exception {
-        Web3j web3 = Web3j.build(new HttpService(
-                "https://rinkeby.infura.io/<your-token>"));
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .authenticator((route, response) -> {
+                    String credential = okhttp3.Credentials.basic("epirus", "epirus-rocks");
+                    return response.request().newBuilder().header("Authorization", credential).build();
+                })
+                .build();
+
+        Web3j web3j = Web3j.build(
+                new HttpService(
+                        "https://rinkby-geth.clients.epirus.blk.io", client, false));
+
 
         Credentials credentials =
                 WalletUtils.loadCredentials(
@@ -22,7 +32,7 @@ public class JaxTokenExample {
                         "<walletfile>");
 
         JaxToken jaxToken = JaxToken.deploy(
-                web3, credentials,
+                web3j, credentials,
                 new StaticGasProvider(DefaultGasProvider.GAS_PRICE,
                         DefaultGasProvider.GAS_LIMIT))
                 .send();
