c72cae171e1cc6caffbed4187fec652be20ba90b
sebaraba <rabasebastians@gmail.com>
2019-10-01 15:49:07 +0100

    Bump web3j to latest version + Clean up comments

--- src/main/java/io/blk/jaxlondon/NetworkVersion.java
+++ src/main/java/io/blk/jaxlondon/NetworkVersion.java
@@ -5,24 +5,27 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.NetVersion;
 import org.web3j.protocol.http.HttpService;
 
-import java.io.IOException;
 
 /**
  * Check network version
  */
 public class NetworkVersion {
 
+    // There is an issue with the root authority we used not being included in version of java prior
+    // to 8u101.
+    // According to https://bugs.openjdk.java.net/browse/JDK-8154757 the IdenTrust CA will be included
+    // in Oracle Java 8u101.]
+    //
+    // Exception in thread "main" javax.net.ssl.SSLHandshakeException: sun.security.validator.ValidatorException:
+    // PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find
+    // valid certification path to requested target
+    //
+    // The certificate required is included in the root repo and can be installed using keytool.
+    // e.g sudo keytool -trustcacerts -keystore
+    // /Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/jre/lib/security/cacerts -storepass
+    // SOMETHING_SECURE -noprompt -importcert -alias lets-encrypt-x3-cross-signed -file lets-encrypt-x3-cross-signed.der
+
     public static void main(String[] args) throws Exception {
-
-// There is an issue with the root authority we used not being included in version of java prior
-// to 8u101.
-// According to https://bugs.openjdk.java.net/browse/JDK-8154757 the IdenTrust CA will be included in Oracle Java 8u101.]
-//
-// Exception in thread "main" javax.net.ssl.SSLHandshakeException: sun.security.validator.ValidatorException: PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target
-//
-// The certificate required is included in the root repo and can be installed using keytool.
-// e.g sudo keytool -trustcacerts -keystore /Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/jre/lib/security/cacerts -storepass SOMETHING_SECURE -noprompt -importcert -alias lets-encrypt-x3-cross-signed -file lets-encrypt-x3-cross-signed.der
-
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .authenticator((route, response) -> {
                     String credential = Credentials.basic("epirus", "epirus-rocks");
@@ -30,11 +33,14 @@ public class NetworkVersion {
                 })
                 .build();
 
+        // Connecting Web3j to a Blockchain Node
         Web3j web3j = Web3j.build(
                 new HttpService(
-                        "https://rinkby-geth.clients.epirus.blk.io", client, false));
+                        "https://epirus:epirus-rocks@geth.epirus.web3labs.com/rinkeby", client, false));
+        // Querying the node for the network version
         NetVersion netVersion =
                 web3j.netVersion().send();
+
         System.out.println("Network Version = " + netVersion.getNetVersion());
     }
 }
