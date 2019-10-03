5d1ab5e8ace172e013ba7b240e7436d9840da80d
sebaraba <rabasebastians@gmail.com>
2019-10-03 15:59:55 +0100

    Remove redudant comment

--- src/main/java/io/blk/jaxlondon/NetworkVersion.java
+++ src/main/java/io/blk/jaxlondon/NetworkVersion.java
@@ -11,20 +11,6 @@ import org.web3j.protocol.http.HttpService;
  */
 public class NetworkVersion {
 
-    // There is an issue with the root authority we used not being included in version of java prior
-    // to 8u101.
-    // According to https://bugs.openjdk.java.net/browse/JDK-8154757 the IdenTrust CA will be included
-    // in Oracle Java 8u101.]
-    //
-    // Exception in thread "main" javax.net.ssl.SSLHandshakeException: sun.security.validator.ValidatorException:
-    // PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find
-    // valid certification path to requested target
-    //
-    // The certificate required is included in the root repo and can be installed using keytool.
-    // e.g sudo keytool -trustcacerts -keystore
-    // /Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/jre/lib/security/cacerts -storepass
-    // SOMETHING_SECURE -noprompt -importcert -alias lets-encrypt-x3-cross-signed -file lets-encrypt-x3-cross-signed.der
-
     public static void main(String[] args) throws Exception {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .authenticator((route, response) -> {
